31bbb386586bb8e9322bdb388660c4c5e05b1af7
Marcin <programowanie.mzwierzchowski@gmail.com>
2022-10-26 12:25:26 +0200

    Intial Project Add

---
+++ src/main/java/com/karaoke/karaokemaker/KaraokemakerApplication.java
@@ -0,0 +1,13 @@
+package com.karaoke.karaokemaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaraokemakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaraokemakerApplication.class, args);
+	}
+
+}
